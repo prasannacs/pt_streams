f88834d9d09c8a39dcb898a4a484712986890be6
Prasanna Selvaraj <prasannas@twitter.com>
2021-09-13 13:41:18 -0400

    startup

--- controllers/stream.js
+++ controllers/stream.js
@@ -35,6 +35,7 @@ router.get("/poll", function (req, res) {
 });
 
 async function streamTweets() {
+  console.log('Streaming Tweets');
   //Listen to the stream
   const options = {
     timeout: 2,
@@ -61,11 +62,11 @@ async function streamTweets() {
         try {
           JSON.parse(json_payload);
           pub_sub_svcs.publishMessage(config.gcp_topicName, JSON.stringify(json_payload));
-          // msg_counter++;
-          // if (msg_counter > config.messageCount) {
-          //   msg_counter = 0;
-          //   pub_sub_svcs.synchronousPull(config.gcp_projectId, config.gcp_subscriptionName, config.messageCount);
-          // }
+          msg_counter++;
+          if (msg_counter > config.messageCount) {
+            msg_counter = 0;
+            pub_sub_svcs.synchronousPull(config.gcp_projectId, config.gcp_subscriptionName, config.messageCount);
+          }
         } catch (e) {
           if (json_payload[0] == undefined || json_payload[0] == '\r' || json_payload[0] == '' || json_payload[0] == '\n') {
             console.log('~~~ Heartbeat payload ~~~ ');
@@ -99,4 +100,5 @@ async function streamTweets() {
   return stream;
 }
 
-module.exports = router;
+module.exports = router
+module.exports.streamTweets = streamTweets;
