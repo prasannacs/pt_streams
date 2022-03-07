a86001717f12e5ea3432844f3f0c4fc5a436e8ab
prasannacs <prasannacs@gmail.com>
2022-03-07 13:17:26 -0500

    media

--- services/bq.js
+++ services/bq.js
@@ -52,7 +52,9 @@ async function insertResults(results, category) {
         }
       } else if (tweet.quoted_status) {
         tweet_type = 'Quote'
-      } else {
+      } 
+      
+      if( tweet_type === null || tweet_type === undefined ) {
         tweet_type = 'Original'
       }
 
@@ -77,37 +79,69 @@ async function insertResults(results, category) {
         entitiesVar.media = [];
       }
 
+      // Determine Media
+      if( tweet.media != undefined) {
+        var media = tweet.media;
+        var mediaArr = getMediaArray(media);
+      }
+      if( tweet.extended_entities != undefined) {
+        var e_media = tweet.extended_entities.media;
+        var e_mediaArr = getMediaArray(e_media);
+        var extended_entities = {};
+        extended_entities.media = e_mediaArr;
+      }
+
       if (tweet.user != undefined) {
         tweet.user.user_url = 'http://twitter.com/' + tweet.user.screen_name
       }
 
       if (tweet.created_at != undefined) {
         var cDate = new Date(tweet.created_at);
+        var tweety = {};
+        tweety.id = tweet.id_str
+        tweety.text = tweet.text
+        tweety.category = category
+        tweety.reply_settings =  tweet.reply_settings
+        tweety.source = tweet.source
+        tweety.author_id = tweet.author_id
+        tweety.conversation_id = tweet.conversation_id
+        tweety.created_at = BigQuery.datetime(cDate.toISOString())
+        tweety.lang = tweet.lang
+        tweety.in_reply_to_user_id = tweet.in_reply_to_user_id
+        tweety.in_reply_to_screen_name = tweet.in_reply_to_screen_name
+        tweety.possibly_sensitive = tweet.possibly_sensitive
+        tweety.geo = geoVar
+        tweety.entities = entitiesVar
+        tweety.user = tweet.user
+        tweety.tweet_url = 'http://twitter.com/twitter/status/' + tweet.id_str
+        tweety.tweet_type = tweet_type
+        if( tweet.media != undefined || tweet.media != null)
+          tweety.media = mediaArr
+        if( tweet.extended_entities != undefined || tweet.extended_entities != null)
+          tweety.extended_entities = extended_entities
         //console.log('====== pushed tweet id ',tweet.id, 'type ', tweet_type);
-        resultRows.push({
-          id: tweet.id_str,
-          text: tweet.text,
-          category: category,
-          reply_settings: tweet.reply_settings,
-          source: tweet.source,
-          author_id: tweet.author_id,
-          conversation_id: tweet.conversation_id,
-          created_at: BigQuery.datetime(cDate.toISOString()),
-          lang: tweet.lang,
-          in_reply_to_user_id: tweet.in_reply_to_user_id,
-          in_reply_to_screen_name: tweet.in_reply_to_screen_name,
-          possibly_sensitive: tweet.possibly_sensitive,
-          geo: geoVar,
-          entities: entitiesVar,
-          user: tweet.user,
-          tweet_url: 'http://twitter.com/twitter/status/' + tweet.id_str,
-          tweet_type: tweet_type
-        });
+        resultRows.push(tweety);
       }
     }
   });
   if (resultRows.length > 0)
     insertRowsAsStream(resultRows);
+}
+
+function getMediaArray(media) {
+  var mediaArr = [];
+  media.forEach(function (media, index) {
+    var mediaObj = {};
+    mediaObj.id = media.id_str;
+    mediaObj.media_url = media.media_url;
+    mediaObj.media_url_https = media.media_url_https;
+    mediaObj.url = media.url;
+    mediaObj.display_url = media.display_url;
+    mediaObj.expanded_url = media.expanded_url;
+    mediaObj.type = media.type;
+    mediaArr.push(mediaObj);
+  })
+  return mediaArr;
 }
 
 module.exports = { insertResults };
