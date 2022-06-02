94eb30d9417f30163eacc4a7b1381333a76dae18
prasannacs <prasannacs@gmail.com>
2022-06-02 16:29:09 -0400

    ui services

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const stream = require('./controllers/stream')
 const api = require('./controllers/api')
+const token = require('./controllers/token')
+const stripe = require('./controllers/stripe')
 
 const app = express();
 const PORT = process.env.PORT || 4020;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 app.options('*', cors()) 
 app.post('*', cors()) 
+
+app.use(bodyParser.json({strict:false}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/stripe', stripe)
 app.use('/stream',stream);
 app.use('/api', api)
+app.use('/token', token)
 
 app.listen(PORT, ()=>   {
     console.log("App listening on port",PORT);
